6086c401fbf0199013055de8b10ded9357b9ad7f
supertamzer <60706571+supertamzer@users.noreply.github.com>
2020-05-20 13:15:48 +0200

    color + number

--- js/sketch.js
+++ js/sketch.js
@@ -9,45 +9,49 @@ function setup() {
 }
 
 function draw() {
-    // Conditionellle nous permettant de savoir si la souris est appuyée
-    // et si le timer est écoulé
-    //if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
-    // Appel de notre fonction generator
-    //generator(width / 2, height / 2, height-150);
 
-    stroke(0)
-    strokeWeight(2)
-    noFill()
-    beginShape();
-    vertex(windowWidth / 2 - 90, 200);
-    vertex(windowWidth / 2 - 230, 240);
-    vertex(windowWidth / 2 - 350, 350);
-    vertex(windowWidth / 2 - 270, 440);
-    vertex(windowWidth / 2 - 190, 370);
-    vertex(windowWidth / 2 - 190, 700);
-    vertex(windowWidth / 2 + 190, 700);
-    vertex(windowWidth / 2 + 190, 370);
-    vertex(windowWidth / 2 + 270, 440);
-    vertex(windowWidth / 2 + 350, 350);
-    vertex(windowWidth / 2 + 230, 240);
-    vertex(windowWidth / 2 + 90, 200);
+    if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
 
+        stroke(255);
+        fill(random(150, 255), random(150, 255), random(150, 255));
+        beginShape();
+        vertex(windowWidth / 2 - 100, 200);
+        vertex(windowWidth / 2 - 230, 240);
+        vertex(windowWidth / 2 - 350, 350);
+        vertex(windowWidth / 2 - 270, 440);
+        vertex(windowWidth / 2 - 190, 370);
+        vertex(windowWidth / 2 - 190, 700);
 
+        vertex(windowWidth / 2 + 190, 700);
+        vertex(windowWidth / 2 + 190, 370);
+        vertex(windowWidth / 2 + 270, 440);
+        vertex(windowWidth / 2 + 350, 350);
+        vertex(windowWidth / 2 + 230, 240);
+        vertex(windowWidth / 2 + 100, 200);
 
+        endShape(CLOSE);
+        generator(width / 2 - 160, height / 2 + 120, 300);
 
-    endShape(CLOSE);
+    }
 }
-//}
 
 // Fonction avec quatres paramètres
-//function generator(x, y, size) {
-// fill utilise le paramètre de notre fonction
-//for (let offset = size; offset > 0; offset -= 10) {
-//fill(random(0,230));
-//rect(x, y, offset, offset);
-//console.log(offset);
-//}
-//}
+
+function generator(x, y, size) {
+
+    fill(random(0, 255), random(0, 150), random(0, 255));
+
+    noStroke();
+    var numbers = []
+    for (var n = 1; n < 99; n++) {
+        if (n <= 9) {
+            n = ("0" + n).slice(-2);
+        }
+        numbers.push(n.toString());
+    }
+    textSize(size)
+    text(random(numbers), x, y)
+}
 
 function mousePressed() {
     // Fonction de librairie P5 nous permettant de sauvegarder une image du canvas
