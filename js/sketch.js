cfddd2ac0cc0d5f049d247d58ae0756c28eb04c6
supertamzer <60706571+supertamzer@users.noreply.github.com>
2020-05-27 10:31:47 +0200

    objets

--- js/sketch.js
+++ js/sketch.js
@@ -5,14 +5,64 @@ let timer = 1;
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-    rectMode(CENTER);
+    maillot = new Maillot(width / 2 - 160, height / 2 + 120, 300, 255);
+
 }
 
 function draw() {
 
     if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
 
-        stroke(255);
+        maillot.shape();
+        maillot.generator();
+
+    }
+}
+
+// Fonction avec quatres paramètres
+
+// function generator(x, y, size) {
+//
+//     fill(random(0, 255), random(0, 150), random(0, 255));
+//
+//     noStroke();
+//     var numbers = []
+//     for (var n = 1; n < 99; n++) {
+//         if (n <= 9) {
+//             n = ("0" + n).slice(-2);
+//         }
+//         numbers.push(n.toString());
+//     }
+//     textSize(size)
+//     text(random(numbers), x, y)
+//
+// }
+
+class Maillot {
+    constructor(x, y, size, stroke) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.stroke = stroke;
+    }
+    generator() {
+
+        fill(random(0, 255), random(0, 150), random(0, 255));
+
+        noStroke();
+        var numbers = []
+        for (var n = 1; n < 99; n++) {
+            if (n <= 9) {
+                n = ("0" + n).slice(-2);
+            }
+            numbers.push(n.toString());
+        }
+        textSize(this.size)
+        text(random(numbers), this.x, this.y)
+
+    }
+    shape() {
+        stroke(this.stroke);
         fill(random(150, 255), random(150, 255), random(150, 255));
         beginShape();
         vertex(windowWidth / 2 - 100, 200);
@@ -30,28 +80,11 @@ function draw() {
         vertex(windowWidth / 2 + 100, 200);
 
         endShape(CLOSE);
-        generator(width / 2 - 160, height / 2 + 120, 300);
-
     }
 }
 
-// Fonction avec quatres paramètres
 
-function generator(x, y, size) {
 
-    fill(random(0, 255), random(0, 150), random(0, 255));
-
-    noStroke();
-    var numbers = []
-    for (var n = 1; n < 99; n++) {
-        if (n <= 9) {
-            n = ("0" + n).slice(-2);
-        }
-        numbers.push(n.toString());
-    }
-    textSize(size)
-    text(random(numbers), x, y)
-}
 
 function mousePressed() {
     // Fonction de librairie P5 nous permettant de sauvegarder une image du canvas
